909dd38d453da2ba72f0a37d173ded55e8740e93
Doğu Kervan <138804269+dogujen@users.noreply.github.com>
2025-08-01 13:50:15 +0300

    Design improvements.

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -1,27 +1,37 @@
-  fetch(`https://api.github.com/users/dogujen/repos?sort=updated`)
-    .then(response => {
-      if (!response.ok) throw new Error("API yanıtı başarısız");
-      return response.json();
-    })
+ fetch(`https://api.github.com/users/dogujen/repos?sort=created`)
+    .then(res => res.json())
     .then(repos => {
-      const list = document.getElementById("repo-list");
-      list.innerHTML = "";
-
-      if (repos.length === 0) {
-        list.innerHTML = "<li>Couldn't find anything.</li>";
-        return;
-      }
+      const container = document.getElementById("repo-container");
+      container.innerHTML = ""; 
 
       repos.forEach(repo => {
-        const item = document.createElement("li");
-        item.innerHTML = `
-          <strong><a href="${repo.html_url}" target="_blank">${repo.name}</a></strong><br>
-          <small>${repo.description ? repo.description : "No description."}</small>
+        const card = document.createElement("article");
+        card.className = "card-wrapper card shadow-sm p-3";
+
+        card.innerHTML = `
+          <a href="${repo.html_url}" class="post-preview row g-0 flex-md-row-reverse text-decoration-none text-dark">
+            <div class="col-md-12">
+              <div class="card-body d-flex flex-column">
+                <h1 class="card-title h4 my-2 mt-md-0">${repo.name}</h1>
+                <div class="card-text content mt-0 mb-3">
+                  <p>${repo.description ? repo.description : "No Description."}</p>
+                </div>
+                <div class="post-meta flex-grow-1 d-flex align-items-end">
+                  <div class="me-auto">
+                    <i class="far fa-star me-1"></i> ⭐ ${repo.stargazers_count}
+                    <i class="far fa-code-branch ms-3 me-1"></i> 🍴 ${repo.forks_count}
+                    <i class="far fa-calendar ms-3 me-1"></i>
+                    <time>${new Date(repo.updated_at).toLocaleDateString()}</time>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </a>
         `;
-        list.appendChild(item);
+        container.appendChild(card);
       });
     })
-    .catch(error => {
-      console.error("Hata:", error);
-      document.getElementById("repo-list").innerHTML = "<li>Error</li>";
+    .catch(err => {
+      console.error(err);
+      document.getElementById("repo-container").innerHTML = '<div class="card p-3">Projects couldn\'t load.</div>';
     });
